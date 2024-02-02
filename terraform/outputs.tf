7a961fa362e3d83b1ac4e9f86c2a74060ea68585
fanat280786 <master97@mail.ru>
2024-02-03 00:17:54 +0300

    убрать хуйню

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -32,5 +32,4 @@ output "dns-fanat-home_id" {
 output "dns-fanat-home_network_mac_addresses" {
   value       = proxmox_virtual_environment_vm.dns-fanat-home.mac_addresses[1]
   description = "dns-fanat-home mac_addresses"
-  хуйня
 }
