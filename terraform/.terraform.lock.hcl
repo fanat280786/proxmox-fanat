39ca8b63c0560b2a345cd5be3cd1291b4d19c91a
fanat280786 <master97@mail.ru>
2024-02-03 00:11:35 +0300

    terraform_pretty

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,26 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/bpg/proxmox" {
+  version     = "0.46.1"
+  constraints = ">= 0.44.0"
+  hashes = [
+    "h1:aU/Kk9oiYYFf9y5p0sMgLFEB3CR3z3/zfQkCZageTsc=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.4.1"
+  constraints = "2.4.1"
+  hashes = [
+    "h1:FzraUapGrJoH3ZOWiUT2m6QpZAD+HmU+JmqZgM4/o2Y=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version     = "4.0.5"
+  constraints = "4.0.5"
+  hashes = [
+    "h1:e4LBdJoZJNOQXPWgOAG0UuPBVhCStu98PieNlqJTmeU=",
+  ]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,18 +1,13 @@
-output "vm_ip" {
+######## fanat-test-ubuntu ###########
+output "fanat-test-ubuntu_ip" {
   value       = proxmox_virtual_environment_vm.fanat-test-ubuntu.ipv4_addresses[1][0]
   description = "Interface IPs"
 }
-
-
-output "vm_id" {
+output "fanat-test-ubuntu_id" {
   value       = proxmox_virtual_environment_vm.fanat-test-ubuntu.vm_id
   description = "Virtual Machine ID"
 }
-
-output "vm_network_interface_name" {
-  value       = proxmox_virtual_environment_vm.fanat-test-ubuntu.network_interface_names[1]
-  description = "Virtual Machine network interface name"
-}
+######## fanat-test-ubuntu ###########
 
 #### fanat-home-assistant #####
 output "fanat-home-assistant_ip" {
@@ -23,11 +18,7 @@ output "fanat-home-assistant_id" {
   value       = proxmox_virtual_environment_vm.fanat-home-assistant.vm_id
   description = "Virtual Machine ID"
 }
-output "fanat-home-assistant_network_interface_name" {
-  value       = proxmox_virtual_environment_vm.fanat-home-assistant.network_interface_names[1]
-  description = "fanat-home-assistant network interface name"
-}
-#### fanat-home-assistant #####
+
 
 #### fanat-DNS #####
 output "dns-fanat-home_ip" {
@@ -38,12 +29,7 @@ output "dns-fanat-home_id" {
   value       = proxmox_virtual_environment_vm.dns-fanat-home.vm_id
   description = "Virtual Machine ID"
 }
-output "dns-fanat-home_network_interface_name" {
-  value       = proxmox_virtual_environment_vm.dns-fanat-home.network_interface_names[1]
-  description = "dns-fanat-home network interface name"
-}
 output "dns-fanat-home_network_mac_addresses" {
   value       = proxmox_virtual_environment_vm.dns-fanat-home.mac_addresses[1]
   description = "dns-fanat-home mac_addresses"
 }
-#### fanat-DNS #####
